52df56f1ad1a22e0de4761c2486c7b56f331ce0b
Simon Restrepo <srestrepo204@gmail.com>
2023-08-02 17:27:28 -0500

    Ejercicio #4 de la actividad participativa

---
+++ punto4.py
@@ -0,0 +1,5 @@
+numero = int(input("Ingrese el numero que requiera validar y presione Enter "))
+if numero % 2 == 0:
+    print("el numero", numero, "es par")
+else:
+    print("el numero", numero, "no es par")
