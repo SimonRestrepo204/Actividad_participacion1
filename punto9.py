a206f075de3442760809c91fd99ae7c8f030c623
Simon Restrepo <srestrepo204@gmail.com>
2023-08-02 18:37:06 -0500

    Ejercicio #9 de la actividad participativa

---
+++ punto9.py
@@ -0,0 +1,31 @@
+from random import randint
+
+def llenar_matriz(n):
+    matriz = []
+
+    for r in range (n):
+        fila = []
+
+        for c in range (n):
+            fila.append(randint(1, 100))
+
+        matriz.append(fila)
+
+    return matriz
+
+resultado = llenar_matriz(4)
+print(resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
