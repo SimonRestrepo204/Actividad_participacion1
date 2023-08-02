3d24bf681b0e5299653e1fab831eaffe6dfe5e9e
Simon Restrepo <srestrepo204@gmail.com>
2023-08-01 21:25:22 -0500

    Ejercicio #1 de la actividad participativa

---
+++ punto1.py
@@ -0,0 +1,4 @@
+Nombre = str(input("Por favor ingresar su nombre completo: "))
+Edad = int(input("Por favor ingresar su edad "))
+print ("El nombre registrado en el sistema es: ", Nombre)
+print ("La edad registrada en el sistema es: ", Edad)
